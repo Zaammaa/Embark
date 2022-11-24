32c2b4ec907fc8a8ccd4acea03daa98fad46b042
krswanson <kristin@swanson.name>
2022-11-24 10:15:27 -0800

    Minor challenge changes based on play-testing

--- app/src/main/java/com/example/embark/Challenges/PreGame/CardDraftingChallenge.kt
+++ app/src/main/java/com/example/embark/Challenges/PreGame/CardDraftingChallenge.kt
@@ -7,7 +7,7 @@ class CardDraftingChallenge(numberOfPlayers: Int, difficulty: Int, game: String)
     override val weight: Int
         get() = 10
     override val difficultyMod: Array<Int>
-        get() = arrayOf(-3,-4,-4)
+        get() = arrayOf(-2,-3,-3)
     override val description: String
         get() = "When dealing, lay aside one card per player. Each crew member picks a card in the same way they pick a task, adding it to their hand. " +
                 "This happens at the same time as selecting tasks if tasks are available. If the 4 ${GameSpecificNames.trump} is one of the cards, the dealer starts the draft."
